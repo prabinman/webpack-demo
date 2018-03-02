06e69afbdc370d7ffa86d6e60c24498b05d7d928
Prabin Shrestha <shrestha.prabinman@gmail.com>
2018-03-02 16:12:46 +0545

    changed index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { cube } from './math.js';
 // import printMe from './print.js';
 // import './styles.css';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 function component() {
     // var element = document.createElement('div');
     var element = document.createElement('pre');
